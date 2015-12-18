02db67909418ee12ac3a49eba8d5ea1edf6fe2e5
Chirag Moradiya <chirag@dreamworldsolutions.com>
2015-12-18 23:07:03 +0530

    rowLimit set to 50

--- src/main/java/com/dw/aristata/export3/WSSClient.java
+++ src/main/java/com/dw/aristata/export3/WSSClient.java
@@ -166,7 +166,7 @@ public class WSSClient {
       
       sb.append("<FieldRef Name=\"" + field.getName() + "\"/>");
     }
-    sb.append("</ViewFields></viewFields><rowLimit>5000</rowLimit>");
+    sb.append("</ViewFields></viewFields><rowLimit>50</rowLimit>");
     sb.append("<queryOptions><QueryOptions xmlns=\"\">" + "<DateInUtc>TRUE</DateInUtc>" + "</QueryOptions>");
     sb.append("</queryOptions>");
     return sb.toString();
