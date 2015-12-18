029b97b89c2f898b23bdbeef8e879bf0485186aa
Chirag Moradiya <chirag@dreamworldsolutions.com>
2015-12-18 23:40:20 +0530

    App updated to retrieve all master lists

--- src/main/java/com/dw/aristata/export3/App.java
+++ src/main/java/com/dw/aristata/export3/App.java
@@ -10,11 +10,21 @@ import com.dw.aristata.export3.dto.ListDef;
 public class App {
   public static void main(String[] args) {
     WSSClient wssClient = new WSSClient();
-    wssClient.writeListAndView(null, "Families", "C:\\aristata-export\\master\\Families-def.xml");
-    wssClient.writeListItems("C:\\aristata-export\\master\\Families-def.xml", null, "Families",
-        "C:\\aristata-export\\master\\Families-data.xml");
-    listDefinitionTest();
+    wssClient.writeListCollection(null, "C:\\aristata-export\\master\\Lists.xml");
+    ListCollectionResult listCollection = XMLParser.parseListCollection("C:\\aristata-export\\master\\Lists.xml");
+    for(ListCollectionResult.Lists.List list : listCollection.getLists().getLists()) {
+      String defFileLoc = "C:\\aristata-export\\master\\" + list.getTitle() + "-def.xml";
+      String dataFileLoc = "C:\\aristata-export\\master\\" + list.getTitle() + "-data.xml";
+      wssClient.writeListAndView(null, list.getTitle(), defFileLoc);
+      wssClient.writeListItems(defFileLoc, null, list.getTitle(), dataFileLoc);
+    }
+//    wssClient.writeListAndView(null, "Families", "C:\\aristata-export\\master\\Families-def.xml");
+//    wssClient.writeListItems("C:\\aristata-export\\master\\Families-def.xml", null, "Families",
+//        "C:\\aristata-export\\master\\Families-data.xml");
+    
+//    listDefinitionTest();
     // listCollectionTest();
+    
   }
 
   private static void listDefinitionTest() {
